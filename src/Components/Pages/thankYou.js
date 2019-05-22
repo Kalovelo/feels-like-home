3c782b09f1c1597fe83f710782b90fe0e69e5293
Prof. Apostoles <apostoloskalovelonis@gmail.com>
2019-05-22 17:00:36 +0300

    scroll cursor feature

--- src/Components/Pages/thankYou.js
+++ src/Components/Pages/thankYou.js
@@ -21,7 +21,7 @@ render(){
                     <Col id='thankYouNames' xs='12' md={{span:'10',offset:'1'}}>
                     
                     <h3 className='thankYouLeft'>My innovative friend <a rel="noopener noreferrer" target="_blank" href='https://www.instagram.com/thanostryfonidis/'>Thanos</a>, for his illustration of mine</h3>
-                    <h3 className='thankYouRight'>My colleague buddy <a rel="noopener noreferrer" target="_blank" href='http://www.martinkaintas.com/'>Martin</a>, for his feedback and tips</h3>
+                    <h3 className='thankYouRight'>My colleague buddies <a rel="noopener noreferrer" target="_blank" href='http://www.martinkaintas.com/'>Martin</a> & <a rel="noopener noreferrer" target="_blank" href='https://www.instagram.com/koutsiamoneys'>Moneys</a>, for their feedback and tips</h3>
                     <h3 className='thankYouLeft'>My 'IEEEdiots' teammates & brothers, <a rel="noopener noreferrer" target="_blank" href='https://gitlab.com/joj0s'>Jojos</a> & <a rel="noopener noreferrer" target="_blank" href='https://github.com/Giapa'>Giapa</a>,  for their feedback and Security tests</h3>
                     <h3 className='thankYouRight'>My friend & advisor <a rel="noopener noreferrer" target="_blank" href='https://github.com/differentreality'>Stella</a>, for her support and creative ideas</h3>
                     <h3 className='thankYouLeft'>Master <a rel="noopener noreferrer" target="_blank" href='https://github.com/dpliakos'>Pliakos</a>, for his guidance on ReactJS</h3>
--- src/apostoloskalovelonis.js
+++ src/apostoloskalovelonis.js
@@ -1,48 +1,91 @@
-  import React, { Component } from 'react';
-  import './App.css';
-  import NavBar from './Components/Navbars/NavBar';
-  import Homepage from './Components/Pages/Homepage/homePage';
-  import Footer from './Components/Pages/Footer';
-  import Background from './Components/Pages/background';
-  import Portal from './Components/Pages/portal';
-  import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-  import About from './Components/Pages/about';
-  import './animations/animations.css';
-  import Cookiefier from './Components/cookiefier';
-  import ThankYou from './Components/Pages/thankYou';
-  import NotFound404 from './Components/Pages/notFound404';
-  import './fonts/font.css'
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './Components/Navbars/NavBar';
+import Homepage from './Components/Pages/Homepage/homePage';
+import Footer from './Components/Pages/Footer';
+import Background from './Components/Pages/background';
+import Portal from './Components/Pages/portal';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import About from './Components/Pages/about';
+import './animations/animations.css';
+import Cookiefier from './Components/cookiefier';
+import ThankYou from './Components/Pages/thankYou';
+import NotFound404 from './Components/Pages/notFound404';
+import './fonts/font.css'
+
+
 
 class Apostoloskalovelonis extends Component {
 
 
- 
 
-  render() {
-    return (
+  componentDidMount() {
+    if(window.innerWidth >= 768){
+      document.getElementById('cursor').setAttribute("style", "opacity:0");
+      document.getElementById('minicursor').setAttribute("style", "opacity:0");
+
+    }
+    }
+
+    handleScroll = (e) => {
+      const scr =parseInt(document.getElementById('cursor').style.top);
+      const miniscr =parseInt(document.getElementById('minicursor').style.top);
+      console.log( "scr = "+document.body.scrollTop+"\n scrollY  = "+window.pageYOffset)
+      if(window.innerWidth >= 768){
+        document.getElementById('cursor').style.top =(window.pageYOffset || document.scrollTop)  - (document.clientTop || 0)+scr/5+"px";
+        document.getElementById('minicursor').style.top =(window.pageYOffset || document.scrollTop)  - (document.clientTop || 0)+miniscr/5+"px";
+        console.log('neo style'+ document.getElementById('cursor').style.top);
+      }
+    }
+
+
+    render(){
+
+      let onMouseMoveH = (e) => {
+        console.log(document.getElementById('cursor').style.top);
+        if(window.innerWidth >= 768){
+        document.getElementById('cursor').setAttribute("style", "top: " + (e.pageY - 10) + "px;left:" + (e.pageX - 10) + "px;");
+        document.getElementById('minicursor').setAttribute("style", "top: " + (e.pageY - 50) + "px;left:" + (e.pageX - 20) + "px;");
+        }
+      }
+
+      let onMouseLeaveH = (e) => {
+        if(window.innerWidth >= 768){
+        document.getElementById('cursor').setAttribute("style", "top: " + (e.pageY - 10) + "px;left:" + (e.pageX - 10) + "px;opacity:0;position:absolute;");
+        document.getElementById('minicursor').setAttribute("style", "top: " + (e.pageY - 20) + "px;left:" + (e.pageX - 20) + "px;opacity:0;position:absolute");
+        }
+      }
+
       
-      <Router >
+
      
-          <div className="App fade-in">
-            <NavBar id='mynav'/>
-            <Cookiefier/>
-            <Switch>
-              <Route exact path='/' component={Homepage}/>
-              <Route path='/Background' component={Background}/>
-              <Route path='/about' component={About}/>
-              <Route path='/thankYou' component={ThankYou}/>
-              <Route path='/portal' component={Portal}/>
-              <Route path='' component={NotFound404}/>
-              
-              
-            </Switch>
-            <Footer/>
-          </div>
-         
-      </Router>
-      
-    );
-  }
-}
 
-export default Apostoloskalovelonis;
+
+      return (
+        <Router>
+          <div id='App' className="App fade-in" onScroll={this.handleScroll} onMouseLeave={onMouseLeaveH} onMouseMove={onMouseMoveH}>
+            {window.innerWidth <= 768 ? '' : <div><div id='cursor' />
+            <div id='minicursor' /> </div>}
+
+            <NavBar id='mynav' />
+            <Cookiefier />
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route path='/Background' component={Background} />
+              <Route path='/about' component={About} />
+              <Route path='/thankYou' component={ThankYou} />
+              <Route path='/portal' component={Portal} />
+              <Route path='' component={NotFound404} />
+
+
+            </Switch>
+            <Footer />
+          </div>
+
+        </Router >
+
+      );
+    }
+  }
+
+  export default Apostoloskalovelonis;
