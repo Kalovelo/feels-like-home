2efd57cb14cd153036d1dc511858928f4249bba1
Kalovelo <apostoloskalovelonis@gmail.com>
2020-02-25 10:17:16 +0200

    Update Cookie Notice

--- src/Components/cookiefier.jsx
+++ src/Components/cookiefier.jsx
@@ -1,46 +1,55 @@
-import React, { Component } from "react"
-import "./cookiefier.css"
-import { faTimes } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { Component, useEffect, useState } from "react"
 import Cookies from "universal-cookie"
-
+import anime from "animejs/lib/anime.es.js"
+import { useRef } from "react"
 const cookie = new Cookies()
 
-class Cookiefier extends Component {
-  constructor() {
-    super()
-    this.state = { shown: cookie.get("cookiefierShown") }
-  }
+const Cookiefier = () => {
+  const [cookieState, setState] = useState(cookie.get("hideCookie"))
 
-  pressedClose = () => {
-    cookie.set("cookiefierShown", "false", { path: "/", maxAge: 2592000 })
-    this.setState(prevState => ({ shown: cookie.get("cookiefierShown") }))
-  }
+  useEffect(() => {
+    floatAnimate()
+  }, [])
 
-  render() {
-    return (
-      <div>
-        {this.state.shown === undefined ? (
-          <div id="cookiefier" xs="10" md="8" lg="2">
-            <FontAwesomeIcon
-              id="cookieButton"
-              onClick={this.pressedClose}
-              className="rotating"
-              icon={faTimes}
-            />
-            <h2>Tea is served with cookies!</h2>
-            <p>
-              In order to keep a functionality and fondness score, I am using Google
-              Analytics to track visitor navigation, so later I can upgrade your
-              visiting experience.{" "}
-            </p>
-          </div>
-        ) : (
-          <div></div>
-        )}
-      </div>
-    )
+  const setCookie = () => {
+    cookie.set("hideCookie", true, { path: "/", maxAge: 2592000 })
+    setState(true)
   }
+  const cookiefierRef = useRef(null)
+
+  const floatAnimate = () =>
+    anime({
+      targets: cookiefierRef.current,
+      loop: true,
+      translateY: [0, 25],
+      duration: 2000,
+      direction: "alternate",
+      easing: "linear"
+    })
+
+  const fadeOut = () =>
+    anime({
+      targets: cookiefierRef.current,
+      opacity: 0,
+      duration: 2000,
+      easing: "linear",
+      complete: setCookie
+    })
+
+  return !cookieState ? (
+    <div className="cookiefier" ref={cookiefierRef}>
+      <h5 className="cookiefier__title">Tea is served with cookies!</h5>
+      <p className="cookiefier__information">
+        In order to keep a functionality and fondness score, I am using Matomo
+        Analytics to track visitor navigation. Worry not, Matomo is open source!{" "}
+      </p>
+      <span onClick={fadeOut} className="cookiefier__cta">
+        Alright!
+      </span>
+    </div>
+  ) : (
+    ""
+  )
 }
 
 export default Cookiefier
