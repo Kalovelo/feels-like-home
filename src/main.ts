af240535f332e8a6cce2c55f736373a724f9020b
Kalovelo <apostoloskalovelonis@gmail.com>
2025-06-29 11:20:55 +0300

    seo

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import { createHead } from '@unhead/vue/client'
 
 import App from './App.vue'
+import { useHead, useSeoMeta } from '@unhead/vue'
 
 const app = createApp(App)
+const head = createHead({})
+
+app.use(head)
+
+useSeoMeta({
+  title: 'Apostolos Kalovelonis',
+  description: 'Personal website of Apostolos Kalovelonis, a software engineer',
+})
 
 app.use(createPinia())
 app.use(PrimeVue, {
